dae9cc5e5e173255782ce86417fc57ac4c55e932
Kiahna Isadore <kiahnaisadore@gmail.com>
2024-07-02 16:40:23 -0700

    completed sorting options
    completed sorting options

--- kudos-board-ui/src/Components/Dashboard/DashboardMain/Dashboard.jsx
+++ kudos-board-ui/src/Components/Dashboard/DashboardMain/Dashboard.jsx
@@ -5,12 +5,14 @@ import CreateBoard from "../CreateBoard/CreateBoard";
 import Board from "../Board/Board";
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
+import Sorting from "../Sorting/Sorting";
 
 
 
 function Dashboard () {
     const [searchInputValue, setSearchInputValue] = useState("");
     const [boardCards, setBoardCards] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("All");
 
     const handleOnSearchInputChange = (event) => {
         setSearchInputValue(event.target.value);
@@ -18,15 +20,21 @@ function Dashboard () {
     const addBoardCard = (newBoardCard) => {
         setBoardCards([...boardCards, newBoardCard]);
     };
-    const filteredBoardCards = boardCards.filter(card =>
-        card.title.toLowerCase().includes(searchInputValue.toLowerCase())
-    );
+    const filteredBoardCards = boardCards.filter(card => {
+        const matchesSearch = card.title.toLowerCase().includes(searchInputValue.toLowerCase());
+        const matchesCategory = selectedCategory == "All" || card.category === selectedCategory;
+        return matchesSearch && matchesCategory;
+    });
     const deleteBoardCard = (id) => {
         const updatedBoardCards = boardCards.filter(card => card.id !== id);
         setBoardCards(updatedBoardCards);
     };
     const viewBoard = (selectedCard) => {
         console.log("View Board:", selectedCard);
+    };
+
+    const handleCateoryChange = (category) => {
+        setSelectedCategory(category);
     };
 
     return(
@@ -36,6 +44,7 @@ function Dashboard () {
                     searchInputValue = {searchInputValue}
                     handleOnSearchInputChange={handleOnSearchInputChange}
                 />
+                <Sorting onCategoryChange={handleCateoryChange}/>
                 <CreateBoard addBoardCard={addBoardCard}/>
 
                 <div className="dashboard-content">
