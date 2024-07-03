a522b097e43e4aa17034f38bfdb1a84f20e8052a
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-07-02 22:11:06 -0700

    highlight gif when selected

--- kudos-board-ui/src/Components/Dashboard/BoardCard/CreateCard/CreateCard.jsx
+++ kudos-board-ui/src/Components/Dashboard/BoardCard/CreateCard/CreateCard.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "../CreateCard/CreateCard.css";
 import KudosCard from "../KudosCard/KudosCard";
 import axios from "axios";
-// import dotenv from "dotenv";
-// dotenv.config();
 
 function CreateCard({ onClose, onAddCard }) {
   const [cardTitle, setCardTitle] = useState("");
@@ -12,7 +10,7 @@ function CreateCard({ onClose, onAddCard }) {
   const [cardAuthor, setCardAuthor] = useState("");
   const [searchGif, setSearchGif] = useState("");
   const [gifs, setGifs] = useState([]);
-  //   const [clickedGifId, setClickedGifId] = useState(null);
+  const [clickedGifId, setClickedGifId] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,15 +29,13 @@ function CreateCard({ onClose, onAddCard }) {
           q: searchGif,
           limit: 6,
           offset: 6,
-          rating: "g",
+          rating: "pg",
           lang: "en",
           bundle: "messaging_non_clips",
         },
       });
-      //   const data = response.json();
-      //   setSearchGifs(response.data.data);
       setGifs(response.data.data);
-      console.log(response.data.data);
+      //   console.log(response.data.data);
     } catch (error) {
       console.error(error.message);
     }
@@ -82,11 +78,10 @@ function CreateCard({ onClose, onAddCard }) {
                     key={gif.id}
                     src={gif.images.fixed_height.url}
                     alt={gif.title}
-                    // className={clickedGifId === gif.id ? "clicked" : ""}
+                    className={clickedGifId === gif.id ? "clicked" : ""}
                     onClick={() => {
                       setGifURL(gif.images.fixed_height.url);
-                      //   setClickedGifId(gif.id);
-                      //   console.log(clickedGifId);
+                      setClickedGifId(gif.id);
                     }}
                   />
                 ))}
