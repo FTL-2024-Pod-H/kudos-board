138e8908ceb8119bcf3cd6c75d567f7e185eb4c4
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-07-03 18:33:40 -0700

    change logo path

--- kudos-board-ui/src/Components/Header/Header.jsx
+++ kudos-board-ui/src/Components/Header/Header.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
 
 const Header = ({ isAuthenticated, onLogout }) => {
-    const navigate = useNavigate();
-    return (
-        <>
-        <div className="header-buttons">
-            <button className="dashboard-button" onClick={() => navigate("/")}>Dashboard</button>
-            {isAuthenticated ? (
-            <button className="sign-in-button" onClick={onLogout}>Sign Out</button>
-            ) : (
-            <button className="sign-in-button" onClick={() => navigate("/login")}>Sign In</button>
-            )}
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="header-buttons">
+        <button className="dashboard-button" onClick={() => navigate("/")}>
+          Dashboard
+        </button>
+        {isAuthenticated ? (
+          <button className="sign-in-button" onClick={onLogout}>
+            Sign Out
+          </button>
+        ) : (
+          <button className="sign-in-button" onClick={() => navigate("/login")}>
+            Sign In
+          </button>
+        )}
       </div>
-        <header className="header">
-            <div className="banner">
-                <img src="../src/assets/images/kudos-logo.png" alt="Kudoboard Logo" className = "header-icon"/>
-            </div>
-        </header>
-        </>
-    );
+      <header className="header">
+        <div className="banner">
+          <img
+            src="/kudos-logo.png"
+            alt="Kudoboard Logo"
+            className="header-icon"
+          />
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
-
